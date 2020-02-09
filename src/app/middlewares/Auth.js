840ba146a6002c43ef1ed34a20520f2078e7e77c
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-02-09 09:45:09 -0300

    Token verification

--- src/app/middlewares/Auth.js
+++ src/app/middlewares/Auth.js
@@ -1,10 +1,25 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
 class Auth {
-  static verifyToken(req, res, next) {
+  static async verifyToken(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       return res.status(401).json({ error: 'Token not provided' });
     }
-    return next();
+    const [, token] = authHeader.split(' ');
+    try {
+      // Usar versão assíncrona pois é mais eficiente
+      const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+      // Seta id do usuário para ser usado nas reqs seguintes
+      req.userId = decoded.id;
+      return next();
+    } catch (error) {
+      // Token inválido
+      return res.status(401).json({ error: 'Invalid token' });
+    }
   }
 }
 
