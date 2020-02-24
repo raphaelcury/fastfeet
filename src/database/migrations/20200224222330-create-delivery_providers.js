266ac6d3230e0c7a4b5e32f3964836ed58f879f9
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-02-24 19:32:45 -0300

    MIgration for delivery providers

---
+++ src/database/migrations/20200224222330-create-delivery_providers.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('delivery_providers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('delivery_providers');
+  },
+};
