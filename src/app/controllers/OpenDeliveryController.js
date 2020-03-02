062ac4764fc5d23db69b1fde459a4e51c1d62b77
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-03-02 12:42:03 -0300

    Checking for max deliveries started per day

--- src/app/controllers/OpenDeliveryController.js
+++ src/app/controllers/OpenDeliveryController.js
@@ -1,6 +1,9 @@
-import { startOfDay, isAfter, isBefore } from 'date-fns';
+import { startOfDay, endOfDay, isAfter, isBefore } from 'date-fns';
+import { Op } from 'sequelize';
 
 import Delivery from '../models/Delivery';
+
+const MAX_DELIVERIES_PER_DAY = 5;
 
 const MIN_HOUR = 8;
 const MAX_HOUR = 18;
@@ -30,10 +33,29 @@ class OpenDeliveryController {
     if (delivery.start_date) {
       return res.status(400).json({ error: 'Delivery has already started' });
     }
-    const now = new Date();
-    const lowerLimit = startOfDay(now).setHours(MIN_HOUR);
-    const upperLimit = startOfDay(now).setHours(MAX_HOUR);
 
+    // Partner can only start MAX_DELIVERIES_PER_DAY deliveries per day
+    const now = new Date();
+    const dayStart = startOfDay(now);
+    const dayEnd = endOfDay(now);
+    const dayDeliveries = await Delivery.findAll({
+      where: {
+        partner_id: req.params.partnerId,
+        start_date: {
+          [Op.between]: [dayStart, dayEnd],
+        },
+      },
+    });
+    console.log(dayDeliveries.length);
+    if (dayDeliveries && dayDeliveries.length >= MAX_DELIVERIES_PER_DAY) {
+      return res.status(400).json({
+        error: `Partner can start only ${MAX_DELIVERIES_PER_DAY} deliveries per day`,
+      });
+    }
+
+    // Deliveries can only start between MIN_HOUR and MAX_HOUR
+    const lowerLimit = dayStart.setHours(MIN_HOUR);
+    const upperLimit = dayStart.setHours(MAX_HOUR);
     if (isBefore(now, lowerLimit) || isAfter(now, upperLimit)) {
       return res.status(400).json({
         error: `Deliveries can only start between ${MIN_HOUR}:00 and ${MAX_HOUR}:00`,
