127f26f4ade42727a1b8678993b9f328ec27ef1e
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-03-14 20:39:54 -0300

    Association between Delivery and Problems: hasMany

--- src/app/models/DeliveryProblem.js
+++ src/app/models/DeliveryProblem.js
@@ -10,13 +10,6 @@ class DeliveryProblem extends Model {
     );
     return this;
   }
-
-  static associate(models) {
-    this.belongsTo(models.Delivery, {
-      foreignKey: 'delivery_id',
-      as: 'delivery',
-    });
-  }
 }
 
 export default DeliveryProblem;
