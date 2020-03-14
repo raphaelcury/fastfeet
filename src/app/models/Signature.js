fa785970e9c339d07515f7c87386bce8e7f5fd52
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-03-14 19:07:54 -0300

    Signature model

---
+++ src/app/models/Signature.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Signature extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `${process.env.SERVER_PROTOCOL}://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/files/signatures/${this.path}`;
+          },
+        },
+      },
+      { sequelize }
+    );
+    return this;
+  }
+}
+
+export default Signature;
